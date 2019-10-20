25789fc3f196985573ab3a2cb77b5083c8d690e6
Mykhailo Lytvyn <mishko.litvin@gmail.com>
2019-10-20 10:33:04 +0300

    Added STM32 source for hardware

---
+++ STM32_Source/voltmeterF100/Debug/Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_ll_usb.d
@@ -0,0 +1,83 @@
+Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_ll_usb.o: \
+ ../Drivers/STM32F1xx_HAL_Driver/Src/stm32f1xx_ll_usb.c \
+ /home/mishko/Dropbox/SharedProgects/STMXCube/voltmeterF100/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal.h \
+ /home/mishko/Dropbox/SharedProgects/STMXCube/voltmeterF100/Inc/stm32f1xx_hal_conf.h \
+ /home/mishko/Dropbox/SharedProgects/STMXCube/voltmeterF100/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_rcc.h \
+ /home/mishko/Dropbox/SharedProgects/STMXCube/voltmeterF100/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_def.h \
+ /home/mishko/Dropbox/SharedProgects/STMXCube/voltmeterF100/Drivers/CMSIS/Device/ST/STM32F1xx/Include/stm32f1xx.h \
+ /home/mishko/Dropbox/SharedProgects/STMXCube/voltmeterF100/Drivers/CMSIS/Device/ST/STM32F1xx/Include/stm32f103xb.h \
+ /home/mishko/Dropbox/SharedProgects/STMXCube/voltmeterF100/Drivers/CMSIS/Include/core_cm3.h \
+ /home/mishko/Dropbox/SharedProgects/STMXCube/voltmeterF100/Drivers/CMSIS/Include/cmsis_version.h \
+ /home/mishko/Dropbox/SharedProgects/STMXCube/voltmeterF100/Drivers/CMSIS/Include/cmsis_compiler.h \
+ /home/mishko/Dropbox/SharedProgects/STMXCube/voltmeterF100/Drivers/CMSIS/Include/cmsis_gcc.h \
+ /home/mishko/Dropbox/SharedProgects/STMXCube/voltmeterF100/Drivers/CMSIS/Device/ST/STM32F1xx/Include/system_stm32f1xx.h \
+ /home/mishko/Dropbox/SharedProgects/STMXCube/voltmeterF100/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_rcc_ex.h \
+ /home/mishko/Dropbox/SharedProgects/STMXCube/voltmeterF100/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_gpio.h \
+ /home/mishko/Dropbox/SharedProgects/STMXCube/voltmeterF100/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_gpio_ex.h \
+ /home/mishko/Dropbox/SharedProgects/STMXCube/voltmeterF100/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_exti.h \
+ /home/mishko/Dropbox/SharedProgects/STMXCube/voltmeterF100/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_dma.h \
+ /home/mishko/Dropbox/SharedProgects/STMXCube/voltmeterF100/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_dma_ex.h \
+ /home/mishko/Dropbox/SharedProgects/STMXCube/voltmeterF100/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_cortex.h \
+ /home/mishko/Dropbox/SharedProgects/STMXCube/voltmeterF100/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_flash.h \
+ /home/mishko/Dropbox/SharedProgects/STMXCube/voltmeterF100/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_flash_ex.h \
+ /home/mishko/Dropbox/SharedProgects/STMXCube/voltmeterF100/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_i2c.h \
+ /home/mishko/Dropbox/SharedProgects/STMXCube/voltmeterF100/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_pwr.h \
+ /home/mishko/Dropbox/SharedProgects/STMXCube/voltmeterF100/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_spi.h \
+ /home/mishko/Dropbox/SharedProgects/STMXCube/voltmeterF100/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_uart.h \
+ /home/mishko/Dropbox/SharedProgects/STMXCube/voltmeterF100/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_pcd.h \
+ /home/mishko/Dropbox/SharedProgects/STMXCube/voltmeterF100/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_ll_usb.h \
+ /home/mishko/Dropbox/SharedProgects/STMXCube/voltmeterF100/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_pcd_ex.h
+
+/home/mishko/Dropbox/SharedProgects/STMXCube/voltmeterF100/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal.h:
+
+/home/mishko/Dropbox/SharedProgects/STMXCube/voltmeterF100/Inc/stm32f1xx_hal_conf.h:
+
+/home/mishko/Dropbox/SharedProgects/STMXCube/voltmeterF100/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_rcc.h:
+
+/home/mishko/Dropbox/SharedProgects/STMXCube/voltmeterF100/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_def.h:
+
+/home/mishko/Dropbox/SharedProgects/STMXCube/voltmeterF100/Drivers/CMSIS/Device/ST/STM32F1xx/Include/stm32f1xx.h:
+
+/home/mishko/Dropbox/SharedProgects/STMXCube/voltmeterF100/Drivers/CMSIS/Device/ST/STM32F1xx/Include/stm32f103xb.h:
+
+/home/mishko/Dropbox/SharedProgects/STMXCube/voltmeterF100/Drivers/CMSIS/Include/core_cm3.h:
+
+/home/mishko/Dropbox/SharedProgects/STMXCube/voltmeterF100/Drivers/CMSIS/Include/cmsis_version.h:
+
+/home/mishko/Dropbox/SharedProgects/STMXCube/voltmeterF100/Drivers/CMSIS/Include/cmsis_compiler.h:
+
+/home/mishko/Dropbox/SharedProgects/STMXCube/voltmeterF100/Drivers/CMSIS/Include/cmsis_gcc.h:
+
+/home/mishko/Dropbox/SharedProgects/STMXCube/voltmeterF100/Drivers/CMSIS/Device/ST/STM32F1xx/Include/system_stm32f1xx.h:
+
+/home/mishko/Dropbox/SharedProgects/STMXCube/voltmeterF100/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_rcc_ex.h:
+
+/home/mishko/Dropbox/SharedProgects/STMXCube/voltmeterF100/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_gpio.h:
+
+/home/mishko/Dropbox/SharedProgects/STMXCube/voltmeterF100/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_gpio_ex.h:
+
+/home/mishko/Dropbox/SharedProgects/STMXCube/voltmeterF100/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_exti.h:
+
+/home/mishko/Dropbox/SharedProgects/STMXCube/voltmeterF100/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_dma.h:
+
+/home/mishko/Dropbox/SharedProgects/STMXCube/voltmeterF100/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_dma_ex.h:
+
+/home/mishko/Dropbox/SharedProgects/STMXCube/voltmeterF100/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_cortex.h:
+
+/home/mishko/Dropbox/SharedProgects/STMXCube/voltmeterF100/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_flash.h:
+
+/home/mishko/Dropbox/SharedProgects/STMXCube/voltmeterF100/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_flash_ex.h:
+
+/home/mishko/Dropbox/SharedProgects/STMXCube/voltmeterF100/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_i2c.h:
+
+/home/mishko/Dropbox/SharedProgects/STMXCube/voltmeterF100/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_pwr.h:
+
+/home/mishko/Dropbox/SharedProgects/STMXCube/voltmeterF100/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_spi.h:
+
+/home/mishko/Dropbox/SharedProgects/STMXCube/voltmeterF100/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_uart.h:
+
+/home/mishko/Dropbox/SharedProgects/STMXCube/voltmeterF100/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_pcd.h:
+
+/home/mishko/Dropbox/SharedProgects/STMXCube/voltmeterF100/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_ll_usb.h:
+
+/home/mishko/Dropbox/SharedProgects/STMXCube/voltmeterF100/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_pcd_ex.h:
---
+++ STM32_Source/voltmeterF100/Debug/Src/usbd_conf.d
@@ -0,0 +1,112 @@
+Src/usbd_conf.o: ../Src/usbd_conf.c \
+ /home/mishko/Dropbox/SharedProgects/STMXCube/voltmeterF100/Drivers/CMSIS/Device/ST/STM32F1xx/Include/stm32f1xx.h \
+ /home/mishko/Dropbox/SharedProgects/STMXCube/voltmeterF100/Drivers/CMSIS/Device/ST/STM32F1xx/Include/stm32f103xb.h \
+ /home/mishko/Dropbox/SharedProgects/STMXCube/voltmeterF100/Drivers/CMSIS/Include/core_cm3.h \
+ /home/mishko/Dropbox/SharedProgects/STMXCube/voltmeterF100/Drivers/CMSIS/Include/cmsis_version.h \
+ /home/mishko/Dropbox/SharedProgects/STMXCube/voltmeterF100/Drivers/CMSIS/Include/cmsis_compiler.h \
+ /home/mishko/Dropbox/SharedProgects/STMXCube/voltmeterF100/Drivers/CMSIS/Include/cmsis_gcc.h \
+ /home/mishko/Dropbox/SharedProgects/STMXCube/voltmeterF100/Drivers/CMSIS/Device/ST/STM32F1xx/Include/system_stm32f1xx.h \
+ /home/mishko/Dropbox/SharedProgects/STMXCube/voltmeterF100/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal.h \
+ /home/mishko/Dropbox/SharedProgects/STMXCube/voltmeterF100/Inc/stm32f1xx_hal_conf.h \
+ /home/mishko/Dropbox/SharedProgects/STMXCube/voltmeterF100/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_rcc.h \
+ /home/mishko/Dropbox/SharedProgects/STMXCube/voltmeterF100/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_def.h \
+ /home/mishko/Dropbox/SharedProgects/STMXCube/voltmeterF100/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_rcc_ex.h \
+ /home/mishko/Dropbox/SharedProgects/STMXCube/voltmeterF100/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_gpio.h \
+ /home/mishko/Dropbox/SharedProgects/STMXCube/voltmeterF100/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_gpio_ex.h \
+ /home/mishko/Dropbox/SharedProgects/STMXCube/voltmeterF100/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_exti.h \
+ /home/mishko/Dropbox/SharedProgects/STMXCube/voltmeterF100/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_dma.h \
+ /home/mishko/Dropbox/SharedProgects/STMXCube/voltmeterF100/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_dma_ex.h \
+ /home/mishko/Dropbox/SharedProgects/STMXCube/voltmeterF100/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_cortex.h \
+ /home/mishko/Dropbox/SharedProgects/STMXCube/voltmeterF100/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_flash.h \
+ /home/mishko/Dropbox/SharedProgects/STMXCube/voltmeterF100/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_flash_ex.h \
+ /home/mishko/Dropbox/SharedProgects/STMXCube/voltmeterF100/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_i2c.h \
+ /home/mishko/Dropbox/SharedProgects/STMXCube/voltmeterF100/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_pwr.h \
+ /home/mishko/Dropbox/SharedProgects/STMXCube/voltmeterF100/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_spi.h \
+ /home/mishko/Dropbox/SharedProgects/STMXCube/voltmeterF100/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_uart.h \
+ /home/mishko/Dropbox/SharedProgects/STMXCube/voltmeterF100/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_pcd.h \
+ /home/mishko/Dropbox/SharedProgects/STMXCube/voltmeterF100/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_ll_usb.h \
+ /home/mishko/Dropbox/SharedProgects/STMXCube/voltmeterF100/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_pcd_ex.h \
+ /home/mishko/Dropbox/SharedProgects/STMXCube/voltmeterF100/Middlewares/ST/STM32_USB_Device_Library/Core/Inc/usbd_def.h \
+ /home/mishko/Dropbox/SharedProgects/STMXCube/voltmeterF100/Inc/usbd_conf.h \
+ /home/mishko/Dropbox/SharedProgects/STMXCube/voltmeterF100/Inc/main.h \
+ /home/mishko/Dropbox/SharedProgects/STMXCube/voltmeterF100/Middlewares/ST/STM32_USB_Device_Library/Core/Inc/usbd_core.h \
+ /home/mishko/Dropbox/SharedProgects/STMXCube/voltmeterF100/Middlewares/ST/STM32_USB_Device_Library/Core/Inc/usbd_def.h \
+ /home/mishko/Dropbox/SharedProgects/STMXCube/voltmeterF100/Middlewares/ST/STM32_USB_Device_Library/Core/Inc/usbd_ioreq.h \
+ /home/mishko/Dropbox/SharedProgects/STMXCube/voltmeterF100/Middlewares/ST/STM32_USB_Device_Library/Core/Inc/usbd_core.h \
+ /home/mishko/Dropbox/SharedProgects/STMXCube/voltmeterF100/Middlewares/ST/STM32_USB_Device_Library/Core/Inc/usbd_ctlreq.h \
+ /home/mishko/Dropbox/SharedProgects/STMXCube/voltmeterF100/Middlewares/ST/STM32_USB_Device_Library/Class/CDC/Inc/usbd_cdc.h \
+ /home/mishko/Dropbox/SharedProgects/STMXCube/voltmeterF100/Middlewares/ST/STM32_USB_Device_Library/Core/Inc/usbd_ioreq.h
+
+/home/mishko/Dropbox/SharedProgects/STMXCube/voltmeterF100/Drivers/CMSIS/Device/ST/STM32F1xx/Include/stm32f1xx.h:
+
+/home/mishko/Dropbox/SharedProgects/STMXCube/voltmeterF100/Drivers/CMSIS/Device/ST/STM32F1xx/Include/stm32f103xb.h:
+
+/home/mishko/Dropbox/SharedProgects/STMXCube/voltmeterF100/Drivers/CMSIS/Include/core_cm3.h:
+
+/home/mishko/Dropbox/SharedProgects/STMXCube/voltmeterF100/Drivers/CMSIS/Include/cmsis_version.h:
+
+/home/mishko/Dropbox/SharedProgects/STMXCube/voltmeterF100/Drivers/CMSIS/Include/cmsis_compiler.h:
+
+/home/mishko/Dropbox/SharedProgects/STMXCube/voltmeterF100/Drivers/CMSIS/Include/cmsis_gcc.h:
+
+/home/mishko/Dropbox/SharedProgects/STMXCube/voltmeterF100/Drivers/CMSIS/Device/ST/STM32F1xx/Include/system_stm32f1xx.h:
+
+/home/mishko/Dropbox/SharedProgects/STMXCube/voltmeterF100/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal.h:
+
+/home/mishko/Dropbox/SharedProgects/STMXCube/voltmeterF100/Inc/stm32f1xx_hal_conf.h:
+
+/home/mishko/Dropbox/SharedProgects/STMXCube/voltmeterF100/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_rcc.h:
+
+/home/mishko/Dropbox/SharedProgects/STMXCube/voltmeterF100/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_def.h:
+
+/home/mishko/Dropbox/SharedProgects/STMXCube/voltmeterF100/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_rcc_ex.h:
+
+/home/mishko/Dropbox/SharedProgects/STMXCube/voltmeterF100/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_gpio.h:
+
+/home/mishko/Dropbox/SharedProgects/STMXCube/voltmeterF100/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_gpio_ex.h:
+
+/home/mishko/Dropbox/SharedProgects/STMXCube/voltmeterF100/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_exti.h:
+
+/home/mishko/Dropbox/SharedProgects/STMXCube/voltmeterF100/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_dma.h:
+
+/home/mishko/Dropbox/SharedProgects/STMXCube/voltmeterF100/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_dma_ex.h:
+
+/home/mishko/Dropbox/SharedProgects/STMXCube/voltmeterF100/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_cortex.h:
+
+/home/mishko/Dropbox/SharedProgects/STMXCube/voltmeterF100/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_flash.h:
+
+/home/mishko/Dropbox/SharedProgects/STMXCube/voltmeterF100/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_flash_ex.h:
+
+/home/mishko/Dropbox/SharedProgects/STMXCube/voltmeterF100/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_i2c.h:
+
+/home/mishko/Dropbox/SharedProgects/STMXCube/voltmeterF100/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_pwr.h:
+
+/home/mishko/Dropbox/SharedProgects/STMXCube/voltmeterF100/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_spi.h:
+
+/home/mishko/Dropbox/SharedProgects/STMXCube/voltmeterF100/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_uart.h:
+
+/home/mishko/Dropbox/SharedProgects/STMXCube/voltmeterF100/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_pcd.h:
+
+/home/mishko/Dropbox/SharedProgects/STMXCube/voltmeterF100/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_ll_usb.h:
+
+/home/mishko/Dropbox/SharedProgects/STMXCube/voltmeterF100/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_pcd_ex.h:
+
+/home/mishko/Dropbox/SharedProgects/STMXCube/voltmeterF100/Middlewares/ST/STM32_USB_Device_Library/Core/Inc/usbd_def.h:
+
+/home/mishko/Dropbox/SharedProgects/STMXCube/voltmeterF100/Inc/usbd_conf.h:
+
+/home/mishko/Dropbox/SharedProgects/STMXCube/voltmeterF100/Inc/main.h:
+
+/home/mishko/Dropbox/SharedProgects/STMXCube/voltmeterF100/Middlewares/ST/STM32_USB_Device_Library/Core/Inc/usbd_core.h:
+
+/home/mishko/Dropbox/SharedProgects/STMXCube/voltmeterF100/Middlewares/ST/STM32_USB_Device_Library/Core/Inc/usbd_def.h:
+
+/home/mishko/Dropbox/SharedProgects/STMXCube/voltmeterF100/Middlewares/ST/STM32_USB_Device_Library/Core/Inc/usbd_ioreq.h:
+
+/home/mishko/Dropbox/SharedProgects/STMXCube/voltmeterF100/Middlewares/ST/STM32_USB_Device_Library/Core/Inc/usbd_core.h:
+
+/home/mishko/Dropbox/SharedProgects/STMXCube/voltmeterF100/Middlewares/ST/STM32_USB_Device_Library/Core/Inc/usbd_ctlreq.h:
+
+/home/mishko/Dropbox/SharedProgects/STMXCube/voltmeterF100/Middlewares/ST/STM32_USB_Device_Library/Class/CDC/Inc/usbd_cdc.h:
+
+/home/mishko/Dropbox/SharedProgects/STMXCube/voltmeterF100/Middlewares/ST/STM32_USB_Device_Library/Core/Inc/usbd_ioreq.h:
